1d0c2f6231f3bba38584eba01e416df8e43f472f
zq19920306 <1113638674@qq.com>
2024-03-14 23:33:56 +0800

    snapshot生成图片

--- src/pages/index.config.js
+++ src/pages/index.config.js
@@ -1,6 +1,8 @@
 export default {
+  renderer: 'skyline',
   navigationBarTitleText: '首页',
   enablePullDownRefresh: false,
   enableShareAppMessage: true,
   enableShareTimeline: true,
+  navigationStyle: 'custom',
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from '@tarojs/components';
+import { View, Text, Snapshot } from '@tarojs/components';
 import Taro, {
 	usePullDownRefresh,
 	useShareAppMessage,
@@ -8,6 +8,39 @@ import Taro, {
 import './index.scss';
 
 const Index = () => {
+	const handleShare = () => {
+		console.log('点击生成图片')
+		Taro.createSelectorQuery()
+			.select('#poemCard')
+			.node()
+			.exec((res) => {
+				console.log(res[0]);
+				const node = res[0].node;
+				node.takeSnapshot({
+					type: 'arraybuffer',
+					format: 'png',
+					success: (res1) => {
+						console.log(res1);
+						const f = `${Taro.env.USER_DATA_PATH}/gushiPoemCard.png`;
+						const fs = Taro.getFileSystemManager();
+						fs.writeFileSync(f, res1.data, 'binary');
+						Taro.showToast({
+							title: '保存成功',
+						});
+
+						Taro.saveImageToPhotosAlbum({
+							filePath: f,
+							complete(res2) {
+								console.log('saveImageToPhotosAlbum:', res2);
+							},
+						});
+					},
+					fail(res1) {
+						console.log(res1);
+					},
+				});
+			});
+	};
 	usePullDownRefresh(() => {
 		Taro.stopPullDownRefresh();
 	});
@@ -25,45 +58,48 @@ const Index = () => {
 	});
 	return (
 		<View className='page homePage'>
-			<View className='poemCard'>
-				<View className='container'>
-					<View className='poem-context'>
-						<View className='author'>
-							<Text className='text'>唐·李白</Text>
+			<Snapshot mode='view' className='poemShot' id='poemCard'>
+				<View className='poemCard'>
+					<View className='container'>
+						<View className='poem-context'>
+							<View className='author-container'>
+								<Text className='text'>唐·李白</Text>
+							</View>
+							<View className='title-container'>
+								<Text className='text'>赠汪伦</Text>
+							</View>
+							<View className='poem'>
+								<View className='poem-text'>
+									<Text className='text'>李白乘舟将欲行</Text>
+								</View>
+								<View className='poem-text'>
+									<Text className='text'>忽闻岸上踏歌声</Text>
+								</View>
+								<View className='poem-text'>
+									<Text className='text'>桃花潭水深千尺</Text>
+								</View>
+								<View className='poem-text'>
+									<Text className='text'>不及汪伦送我情</Text>
+								</View>
+							</View>
 						</View>
-						<View className='title'>
-							<Text className='text'>赠汪伦</Text>
+					</View>
+					<View className='bottom'>
+						<View className='date'>
+							<View className='nongli'>甲辰年 二月初四</View>
+							<View className='yangli'>2024/03/13</View>
 						</View>
-						<View className='poem'>
-							<View className='poem-text'>
-								<Text className='text'>李白乘舟将欲行</Text>
-							</View>
-							<View className='poem-text'>
-								<Text className='text'>忽闻岸上踏歌声</Text>
-							</View>
-							<View className='poem-text'>
-								<Text className='text'>桃花潭水深千尺</Text>
-							</View>
-							<View className='poem-text'>
-								<Text className='text'>不及汪伦送我情</Text>
+						<View className='desc'>
+							<View className='yi'>宜平安喜乐·出行</View>
+							<View className='share' onClick={handleShare}>
+								<View className='at-icon at-icon-heart'></View>
+								<View className='at-icon at-icon-share'></View>
 							</View>
 						</View>
 					</View>
 				</View>
-				<View className='bottom'>
-					<View className='date'>
-						<View className='nongli'>甲辰年 二月初四</View>
-						<View className='yangli'>2024/03/13</View>
-					</View>
-					<View className='desc'>
-						<View className='yi'>宜平安喜乐·出行</View>
-						<View className='share'>
-							<View className='at-icon at-icon-heart'></View>
-							<View className='at-icon at-icon-share'></View>
-						</View>
-					</View>
-				</View>
-			</View>
+			</Snapshot>
+			<View className='outShare' onClick={handleShare}>分享生成图片</View>
 		</View>
 	);
 };
