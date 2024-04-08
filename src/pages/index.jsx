f175f25b2580d10bb7b86e27817bda12f093bcab
zq19920306 <1113638674@qq.com>
2024-04-08 23:37:46 +0800

    画报配置组件拆分

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
-import { View, Text, Snapshot, Image, ScrollView } from '@tarojs/components';
+import { View, Text, Snapshot, Image } from '@tarojs/components';
 import { useState, useEffect, useCallback } from 'react';
-import { AtButton } from 'taro-ui';
 import Taro, {
 	useShareAppMessage,
 	useShareTimeline,
@@ -10,10 +9,8 @@ import Taro, {
 } from '@tarojs/taro';
 
 import xcxPng from '../images/xcx.jpg';
-import Qrcode from '../images/icon/qrcode.png';
 import shareSvg from '../images/svg/share.svg';
 import refreshSvg from '../images/svg/refresh.svg';
-import saveSvg from '../images/svg/save.svg';
 import Utils from '../utils/util';
 import { fetchRandomSentence } from '../services/global';
 import { postBgImages, FontFaceList } from '../const/config';
@@ -22,7 +19,7 @@ import LoadLocalFont from '../utils/loadFont';
 import Layout from '../layout';
 import PoemPostCard from '../components/PoemPost';
 import FloatLayout from '../components/FloatLayout';
-import PoemPostLayout from '../components/Skeleton/PoemPostLayout';
+import PostLayoutConfig from '../components/PostLayoutConfig';
 
 import './index.scss';
 
@@ -42,44 +39,6 @@ const splitSentence = (sentence) => {
 		.reverse();
 };
 
-// 边框颜色配置
-const letterLayoutConfig = [
-	{
-		name: 'default',
-		color: '#333',
-	},
-	{
-		name: 'center',
-		color: '#333',
-	},
-	{
-		name: 'blackBorder',
-		color: '#212321',
-	},
-	{
-		name: 'redBorder',
-		color: '#c01112',
-	},
-];
-
-// 字体颜色
-const fontColorArr = ['#fff', '#333'];
-
-// 模式
-const ratioConfig = [
-	{
-		name: '默认',
-		value: 1,
-	},
-	{
-		name: '小红书',
-		value: 0.75,
-	},
-	{
-		name: '手机壁纸',
-		value: 0.4615,
-	},
-];
 const Index = () => {
 	const [date] = useState(() => {
 		return Utils.formatDate().join('/');
@@ -115,45 +74,6 @@ const Index = () => {
 	const handleReload = () => {
 		updateReload(true);
 		fetchSentence(true);
-	};
-
-	const handleToggleBottom = () => {
-		updateConfig({
-			...postConfig,
-			showQrcode: !postConfig.showQrcode,
-		});
-	};
-
-	const updateLayout = ({ type, letterBorder }) => {
-		updateConfig({
-			...postConfig,
-			type: type,
-			letterBorder: letterBorder || '',
-		});
-	};
-
-	const selectFontColor = (e) => {
-		const { fontColor } = e.currentTarget.dataset;
-		updateConfig({
-			...postConfig,
-			fontColor: fontColor || '#333',
-		});
-	};
-
-	const selectBgImg = (e) => {
-		const { img } = e.currentTarget.dataset;
-		updateConfig({
-			...postConfig,
-			bgImg: img,
-		});
-	};
-
-	const selecrRatio = (e) => {
-		const { ratio } = e.currentTarget.dataset;
-		updateConfig({
-			...postConfig,
-			ratio: ratio || 0.75,
-		});
 	};
 
 	const fetchSentence = useCallback((forceGet = false) => {
@@ -383,180 +303,12 @@ const Index = () => {
 						visibility: isOpen ? 'visible' : 'hidden',
 					}}
 				>
-					<View className='post-config'>
-						{/* 布局 */}
-						<View className='shareLayout'>
-							<View className='title'>
-								<Text className='text'>布局</Text>
-							</View>
-							<View className='layout-bottom'>
-								<View className='scrollContainer'>
-									{letterLayoutConfig.map((layout) => {
-										return (
-											<PoemPostLayout
-												type={layout.name}
-												key={layout.name}
-												style={{
-													width: 30,
-													height: 40,
-													marginRight: 10,
-													borderColor: layout.color,
-												}}
-												borderColor={layout.color}
-												letterBorder={layout.name}
-												update={updateLayout}
-												activeType={postConfig.letterBorder}
-											/>
-										);
-									})}
-								</View>
-								<View
-									className={`qrcode-container  ${
-										postConfig.showQrcode ? 'active' : ''
-									}`}
-									onClick={handleToggleBottom}
-								>
-									<Image
-										src={Qrcode}
-										className='qrcode'
-										mode='widthFix'
-										style={{
-											height: 25,
-											width: 25,
-										}}
-									/>
-								</View>
-							</View>
-						</View>
-						{/* 模式，小红书和壁纸 */}
-						<View className='shareLayout'>
-							<View className='title'>
-								<Text className='text'>展示模式</Text>
-							</View>
-							<View className='scrollContainer ratio-list'>
-								{ratioConfig.map((ratio) => {
-									return (
-										<View
-											key={ratio.value}
-											data-ratio={ratio.value}
-											onClick={selecrRatio}
-											className={`ratio-item ${
-												postConfig.ratio == ratio.value ? 'active' : ''
-											}`}
-										>
-											{ratio.name}
-										</View>
-									);
-								})}
-							</View>
-						</View>
-						{/* 字体颜色 */}
-						<View className='shareLayout'>
-							<View className='title'>
-								<Text className='text'>字体颜色</Text>
-							</View>
-							<View className='layout-bottom'>
-								<View
-									className='scrollContainer'
-									style={{
-										width: '100%',
-									}}
-								>
-									{fontColorArr.map((color) => {
-										return (
-											<View
-												key={color}
-												className={`color-item bgColor ${
-													postConfig.fontColor === color ? 'active' : ''
-												}`}
-												style={{
-													backgroundColor: color,
-													width: 30,
-													height: 30,
-													padding: 4,
-													marginRight: 10,
-												}}
-												data-fontColor={color || ''}
-												onClick={selectFontColor}
-											></View>
-										);
-									})}
-								</View>
-							</View>
-						</View>
-						{/* 背景图 */}
-						<View className='shareLayout'>
-							<View className='title'>
-								<Text className='text'>背景色</Text>
-							</View>
-							<View className='layout-bottom'>
-								<ScrollView
-									scrollX
-									enableFlex
-									enhanced
-									showScrollbar={false}
-									className='scrollContainer bgImgList'
-									style={{
-										height: 52,
-										width: safeArea.width - 30,
-									}}
-								>
-									{postBgImages.map((img) => {
-										return (
-											<View
-												key={img}
-												className={`color-item bgImg ${
-													postConfig.bgImg === img ? 'active' : ''
-												}`}
-												style={{
-													width: 30,
-													height: 30,
-													marginRight: 8,
-												}}
-												data-img={img}
-												onClick={selectBgImg}
-											>
-												<Image
-													src={img}
-													mode='widthFix'
-													className='bg-img'
-													style={{
-														width: 30,
-														height: 30,
-													}}
-												/>
-											</View>
-										);
-									})}
-								</ScrollView>
-							</View>
-						</View>
-						{/* 底部按钮 */}
-						<View className='shareBottom'>
-							{!isPc ? (
-								<AtButton
-									className='share-btn'
-									type='primary'
-									size='small'
-									circle
-									onClick={handleDownload}
-								>
-									<Image src={saveSvg} mode='widthFix' className='icon' />
-									<Text className='text'>保存</Text>
-								</AtButton>
-							) : null}
-							<AtButton
-								className='share-btn'
-								type='secondary'
-								size='small'
-								circle
-								openType='share'
-							>
-								<Image src={shareSvg} mode='widthFix' className='icon' />
-								<Text className='text'>分享</Text>
-							</AtButton>
-						</View>
-					</View>
+					<PostLayoutConfig
+						safeArea={safeArea}
+						isPc={isPc}
+						update={updateConfig}
+						handleDownload={handleDownload}
+					/>
 				</FloatLayout>
 			</View>
 		</Layout>
