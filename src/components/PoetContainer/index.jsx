4e0fa2c7d8b5307ee81ec329803d3ec52f819601
zq19920306 <1113638674@qq.com>
2025-03-23 18:23:32 +0800

    update:文库页更新

--- src/components/PoetContainer/index.jsx
+++ src/components/PoetContainer/index.jsx
@@ -2,19 +2,42 @@ import { useState, useEffect, useRef } from 'react';
 import Taro from '@tarojs/taro';
 import { View, Text, ScrollView } from '@tarojs/components';
 
-import PoetCard from "../../components/PoetCard";
+import PoetSmallCard from '../../components/PoetSmallCard';
 
 import './style.scss';
-
+import { DynastyArr } from '../../const/config';
 import { fetchPoetData } from '../../pages/poet/service';
 
+const DynastyItem = (props) => {
+	const { handleClick, dynasty, currentDynasty } = props;
+	const dynastyClick = () => {
+		handleClick(dynasty);
+	};
+	let isActive = false;
+	if (['精选','全部'].includes(currentDynasty) && dynasty == '精选') {
+		isActive = true;
+	}
+	if (currentDynasty == dynasty) {
+		isActive = true;
+	}
+	return (
+		<View
+			className={`dynastyItem ${isActive ? 'active' : ''}`}
+			onClick={dynastyClick}
+		>
+			<Text>{dynasty}</Text>
+		</View>
+	);
+};
 const PoetContainer = () => {
 	const pagination = useRef({
 		page: 1,
-		size: 15,
+		size: 20,
 		total: 0,
 		last_page: 2,
 	});
+
+	const dynastyRef = useRef('全部');
 	const refreshFlag = useRef(false);
 	const [poetList, setList] = useState([]);
 	const [error, setError] = useState('');
@@ -23,36 +46,43 @@ const PoetContainer = () => {
 	const reachBottom = () => {
 		console.log('--rearchBottom');
 		const { page, last_page } = pagination.current;
-		if (page < last_page) {
+		if (page <= last_page) {
 			pagination.current.page = page + 1;
 		}
 		Taro.nextTick(() => {
 			fetchList();
 		});
 	};
-	const pullDownRefresh = () => {
-		console.log('page-pullRefresh');
-		// 改变分页数据，自动触发 useFetchList hook
+
+	const handleClick = (dynasty) => {
+		dynastyRef.current = dynasty;
+		console.log('----', dynasty)
 		pagination.current = {
+			...pagination.current,
 			page: 1,
-			size: 15,
-			total: 0,
-			last_page: -1,
+			last_page: 2,
 		};
-		Taro.nextTick(() => {
-			fetchList();
-		});
+		fetchList();
 	};
+
 	const fetchList = () => {
 		if (refreshFlag.current) {
 			return false;
 		}
+		const dynasty = dynastyRef.current == '精选' ? '' : dynastyRef.current;
+		const params = {
+			...pagination.current,
+			dynasty: dynasty,
+		};
 		const { page, last_page: lastPage } = pagination.current;
-		if (page >= lastPage) {
+		if (dynastyRef.current == '全部' && page > 1) {
+			return false;
+		}
+		if (page > lastPage) {
 			return false;
 		}
 		refreshFlag.current = true;
-		fetchPoetData('GET', pagination.current)
+		fetchPoetData('GET', params)
 			.then((res) => {
 				if (res.data && res.statusCode == 200) {
 					const { list = [], current_page, last_page, total } = res.data;
@@ -95,7 +125,37 @@ const PoetContainer = () => {
 
 	return (
 		<View className='poetContainer' id='poetScrollContainer'>
-			{/* 诗人列表 */}
+			{/* 左侧：朝代筛选 */}
+			<ScrollView
+				className='dynastyContainer'
+				scrollY
+				enableFlex
+				enhanced
+				showScrollbar={false}
+				enableBackToTop
+				refresherEnabled={false}
+				style={{
+					height: scrollHeight == 'auto' ? scrollHeight : scrollHeight + 'px',
+				}}
+			>
+				<DynastyItem
+					key='精选'
+					handleClick={handleClick}
+					currentDynasty={dynastyRef.current}
+					dynasty='精选'
+				/>
+				{DynastyArr.map((item) => {
+					return (
+						<DynastyItem
+							key={item}
+							handleClick={handleClick}
+							currentDynasty={dynastyRef.current}
+							dynasty={item}
+						/>
+					);
+				})}
+			</ScrollView>
+			{/* 右侧：诗人列表 */}
 			<ScrollView
 				className='scrollContainer'
 				scrollY
@@ -103,16 +163,15 @@ const PoetContainer = () => {
 				enhanced
 				showScrollbar={false}
 				enableBackToTop
-				refresherEnabled
+				refresherEnabled={false}
 				onScrollToLower={reachBottom}
-				onRefresherPulling={pullDownRefresh}
 				style={{
 					height: scrollHeight == 'auto' ? scrollHeight : scrollHeight + 'px',
 				}}
 			>
 				{poetList.map((item) => {
 					return (
-						<PoetCard
+						<PoetSmallCard
 							{...item}
 							showCount
 							showBorder
--- src/const/config.js
+++ src/const/config.js
@@ -1067,7 +1067,7 @@ export const PoemTypes = ['全部', '诗', '词', '曲', '文言文'];
 
 // 朝代列表
 export const DynastyArr = [
-	'全部',
+	// '全部',
 	'先秦',
 	'两汉',
 	'魏晋',
