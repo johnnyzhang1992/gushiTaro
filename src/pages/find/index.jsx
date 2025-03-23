65c69dba32b5e176e8e14243e82d2d12ad775658
zq19920306 <1113638674@qq.com>
2025-03-23 15:27:06 +0800

    update:细节调整

--- src/pages/find/index.jsx
+++ src/pages/find/index.jsx
@@ -3,8 +3,6 @@ import { View } from '@tarojs/components';
 // import { useState } from 'react';
 import Taro, { useLoad, usePullDownRefresh } from '@tarojs/taro';
 
-// import { HomeBooks, HomeCategories } from '../../const/config';
-// import BookCover from '../../components/BookCover';
 import PageHeader from '../../components/PageHeader';
 import RandomSearch from '../search/components/RandomSearch';
 
@@ -22,52 +20,6 @@ const PostPage = () => {
 		<View className='page findPage'>
 			<PageHeader title='发现' />
 			<View className='divide' />
-			{/* <HomeNavs /> */}
-			{/* 课本 */}
-			{/* <View className='sectionCard'>
-				<View className='cardTitle'>学习</View>
-				<View className='cardContent'>
-					<ScrollView
-						enableFlex
-						scrollX
-						enhanced
-						className='scroll-container'
-						style={{
-							width: 'calc(100vw - 20px)',
-							height: 140
-						}}
-					>
-						{HomeBooks.map((item) => (
-							<BookCover key={item.code} {...item} />
-						))}
-					</ScrollView>
-				</View>
-			</View> */}
-			{/* 选集 */}
-			{/* <View
-				className='sectionCard'
-				style={{
-					padding: 10,
-				}}
-			>
-				<View className='cardTitle'>选集</View>
-				<View className='cardContent'>
-					<ScrollView
-						enableFlex
-						scrollX
-						enhanced
-						className='scroll-container'
-						style={{
-							width: 'calc(100vw - 20px)',
-							height: 140
-						}}
-					>
-						{HomeCategories.map((item) => (
-							<BookCover key={item.code} {...item} />
-						))}
-					</ScrollView>
-				</View>
-			</View> */}
 			{/* 随机探索 */}
 			<RandomSearch />
 			{/* 词牌 */}
