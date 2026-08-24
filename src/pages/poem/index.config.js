a6fc50b0148c2438f73d9bbcd0e34703bee1b688
小小梦工场 <1113638674@qq.com>
2026-08-25 01:19:09 +0800

    fix: 三个列表页添加custom navigation

--- src/pages/poem/index.config.js
+++ src/pages/poem/index.config.js
@@ -1,4 +1,7 @@
 export default {
 	enableShareAppMessage: true,
 	enableShareTimeline: true,
+	navigationBarTextStyle: 'white',
+	navigationBarBackgroundColor: '#337ab7',
+	navigationStyle: 'custom',
 };
--- src/pages/sentence/index.config.js
+++ src/pages/sentence/index.config.js
@@ -2,4 +2,7 @@ export default {
 	navigationBarTitleText: '名句 | 古诗文小助手',
 	enableShareAppMessage: true,
 	enableShareTimeline: true,
-  };
+  	navigationBarTextStyle: 'white',
+	navigationBarBackgroundColor: '#337ab7',
+	navigationStyle: 'custom',
+};
