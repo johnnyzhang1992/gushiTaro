8f0379f62855d844c70121753bfd2b5402d7860f
小小梦工场 <1113638674@qq.com>
2026-08-25 00:53:40 +0800

    fix: 添加诗人详情页 config（custom navigation）

--- src/pages/poet/detail.config.js
+++ src/pages/poet/detail.config.js
@@ -1,4 +1,6 @@
 export default {
-	enableShareAppMessage: true,
-	enableShareTimeline: true,
+	navigationBarTitleText: '诗人详情',
+	navigationBarTextStyle: 'white',
+	navigationBarBackgroundColor: '#337ab7',
+	navigationStyle: 'custom',
 };
